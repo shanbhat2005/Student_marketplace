61bc0072c6533a4bb84ea9fe6147ce5671d3e14c
shan <shanbhat2005@gmail.com>
2026-02-10 01:11:08 +0530

    backend throwing error

--- backend/models/User.js
+++ backend/models/User.js
@@ -25,16 +25,11 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving (only if modified)
-userSchema.pre('save', async function (next) {
-  try {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare login password with hashed password
